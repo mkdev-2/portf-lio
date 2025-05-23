0d59c1e571740dd0f17674dd166362caab9a1167
Marcos Amorim <126255748+mkdev-2@users.noreply.github.com>
2025-05-23 11:43:50 -0300

    fix: Atualiza configuração de build para garantir cópia de imagens e cache correto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import About from '../components/About';
 import Experience from '../components/Experience';
-import Header from '../components/Header';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Hero from '../components/Hero';
-import Service from '../components/Service';
 import HomePagdData from '../data/HomePagdData.json';
 
 export default function Home() {
   const {
     hero,
     socialBtns,
-    brands,
     about,
     hardSkills,
     softSkills,
